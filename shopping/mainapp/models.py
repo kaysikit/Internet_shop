c316476c4a6577b84f49a5c5bd55fa32a5530ed7
kaysikit <kaysik_it@mail.ru>
2021-11-09 21:33:12 +0500

    Mixin for cart

--- shopping/mainapp/models.py
+++ shopping/mainapp/models.py
@@ -126,8 +126,8 @@ class Cart(models.Model):
 
 class Customer(models.Model):
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
-    phone = models.CharField(max_length=20, verbose_name='Номер телефона')
-    address = models.CharField(max_length=255, verbose_name='Адрес')
+    phone = models.CharField(max_length=20, verbose_name='Номер телефона', null=True, blank=True)
+    address = models.CharField(max_length=255, verbose_name='Адрес', null=True, blank=True)
 
     def __str__(self):
         return "Покупатель: {} {}".format(self.user.first_name, self.user.last_name)
